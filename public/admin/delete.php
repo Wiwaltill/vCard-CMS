0339f03d125d6bb2fdb1ee88dc1ef397ac67b293
TiLL Esser <74987472+Wiwaltill@users.noreply.github.com>
2026-05-11 12:19:39 +0200

    Create delete.php

---
+++ public/admin/delete.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once '../includes/functions.php';
+require_login();
+
+$contacts = load_json('contacts.json', []);
+$id = $_GET['id'] ?? '';
+
+foreach ($contacts as $contact) {
+    if (($contact['id'] ?? '') === $id && !empty($contact['bild'])) {
+        delete_public_file($contact['bild']);
+        break;
+    }
+}
+
+$contacts = array_filter($contacts, function ($contact) use ($id) {
+    return ($contact['id'] ?? '') !== $id;
+});
+
+save_contacts($contacts);
+
+header('Location:index.php');
+exit;
