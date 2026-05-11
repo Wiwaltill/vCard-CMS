58c8d2dcd8b7ed86c504ae6bdd714f9044b4539c
TiLL Esser <74987472+Wiwaltill@users.noreply.github.com>
2026-05-11 16:14:06 +0200

    Update sw.php

--- public/sw.php
+++ public/sw.php
@@ -1,4 +1,18 @@
 <?php header('Content-Type: application/javascript; charset=utf-8'); ?>
-const CACHE='vcard-pwa-v1';
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/','/manifest.webmanifest']))));
-self.addEventListener('fetch', e => e.respondWith(fetch(e.request).catch(() => caches.match(e.request))));
+const CACHE='vcard-pwa-v3';
+const CORE=['/manifest.webmanifest','/assets/icons/icon.svg'];
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)));
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(fetch(event.request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE).then(cache => cache.put(event.request, copy)).catch(()=>{});
+    return response;
+  }).catch(() => caches.match(event.request)));
+});
