a3c5081d1c8aa9dd1baebd488a4256e1d2921d64
TiLL Esser <74987472+Wiwaltill@users.noreply.github.com>
2026-05-11 12:20:03 +0200

    Update vcard.php

--- public/vcard.php
+++ public/vcard.php
@@ -1,22 +1,30 @@
 <?php
 
-$contacts = json_decode(file_get_contents('../data/contacts.json'), true);
+require_once 'includes/functions.php';
+
+$config = get_config();
+$contacts = load_json('contacts.json', []);
 
 $id = $_GET['id'] ?? '';
 
 foreach ($contacts as $contact) {
 
-    if ($contact['id'] === $id) {
+    if (($contact['id'] ?? '') === $id) {
 
         $vcard = "BEGIN:VCARD\r\n";
         $vcard .= "VERSION:3.0\r\n";
         $vcard .= "FN:{$contact['vorname']} {$contact['nachname']}\r\n";
-        $vcard .= "ORG:{$contact['firma']}\r\n";
+        $vcard .= "ORG:{$config['company_name']}\r\n";
+
+        if (!empty($contact['position'])) {
+            $vcard .= "TITLE:{$contact['position']}\r\n";
+        }
+
         $vcard .= "TEL:{$contact['telefon']}\r\n";
         $vcard .= "EMAIL:{$contact['email']}\r\n";
         $vcard .= "END:VCARD\r\n";
 
-        header('Content-Type: text/vcard');
+        header('Content-Type: text/vcard; charset=utf-8');
         header('Content-Disposition: attachment; filename="'.$id.'.vcf"');
 
         echo $vcard;
